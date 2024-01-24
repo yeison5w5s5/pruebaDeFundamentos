b5edd170f13990ce8ff284fab270da46bb44e00b
yeison5w5s5 <104599687+yeison5w5s5@users.noreply.github.com>
2024-01-24 12:19:26 -0500

    Refactor 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,10 +1,7 @@
-voltajes=[]
 promedio=0
 for i in range(3):
     voltaje=int(input(f"Ingresa el voltaje {i+1}: "))
-    voltajes.append(voltaje)
-for v in voltajes:
-    promedio+=v
+    promedio+=voltaje
 promedio/=3
 if promedio<150:
     print(f"VOLTAJE CORRECTO= {promedio}")
