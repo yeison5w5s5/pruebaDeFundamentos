43fb2461ed7f62ef8ecf8d1cd4670ae525688817
yeison5w5s5 <104599687+yeison5w5s5@users.noreply.github.com>
2024-01-24 11:54:56 -0500

    ejersicio 1 completado

--- ejersico1.py
+++ ejersico1.py
@@ -1,2 +1,12 @@
 voltajes=[]
-for i in range
+promedio=0
+for i in range(5):
+    voltaje=int(input(f"Ingresa el voltaje {i+1}: "))
+    voltajes.append(voltaje)
+for v in voltajes:
+    promedio+=v
+promedio/=5
+if promedio>220:
+    print(f"ALTO VOLTAJE= {promedio}")
+else:
+    print(f"VOLTAJE CORRECTO= {promedio}")
